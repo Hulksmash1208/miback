66b964d2a5d414354f4099509b8862c0f88490dc
Carlos <carloshurcam@gmail.com>
2018-07-24 20:38:16 -0500

    Arregrle mi desmadre

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 let axios = require ('axios');
 let app = require ('express')();
+
+let port = process.env.PORT || 5000;
+
+
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-app.get ('/', (req,res)=>{
+app.get ('/pokemons', (req,res)=>{
 
 let pokeIds= [3,6,9,130];
 
@@ -28,6 +32,6 @@ Promise.all (promesasIncompletas).then((pokemones)=>{
 });
 
 });
-app.listen(5001, ()=>{
-    console.log('Todo bien, eres el mejor');
+app.listen(port, ()=>{
+    console.log(`Todo bien, eres el mejor`, `me levante en el puerto ${port}`);
 });
